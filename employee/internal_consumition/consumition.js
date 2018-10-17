821227cf0d0994e663c62908b66ac21570a1c52e
mrer100 <jfsosa06@gmail.com>
2018-10-17 10:16:52 -0300

    version de maquetado

--- employee/internal_consumition/consumition.js
+++ employee/internal_consumition/consumition.js
@@ -1,29 +1,32 @@
 (function () {
 	var controllerId = 'app.views.employee.consumition';
 	angular.module('app').controller(controllerId, [
-		'$scope',        
-		'gtObject',
-		'gtUI',
-		'gtPlataform', 
-		'gtMessage',
-		'gtString',		      
-		function ($scope, gtObject,gtUI,gtPlataform,gtMessage,gtString) {
-			var vm = this;	
-        
-            vm.gofrom= function(from,to){
-                $('#'+from).hide();;
-                $('#'+to).show();;
-               
+		'$scope',
+		'gtObject',		
+		'gtString',
+		function ($scope, gtObject, gtString) {
+			var vm = this;
+			
+			vm.consumitionList = [];
 
-            }
+			function initConsumitionList(){
+				var consumitionList=[
+					{id:1,date:"15/10/2017",monto:"$ 137.50",customer:2},
+					{id:3,date:"15/10/2017",monto:"$ 120.00",customer:3},
+					{id:10,date:"16/10/2017",monto:"$ 100.00",customer:2},
+					{id:11,date:"16/10/2017",monto:"$ 147.50",customer:3},
+					{id:14,date:"16/10/2017",monto:"$ 110.00",customer:4}
+				];
+				vm.consumitionList = consumitionList;
+				
+			}
+			function initUI(){
+				initConsumitionList();
+			}
 
-            function initUI(){
-                console.log("consumition");
+			initUI();
+			
+		}
 
-            }
-            initUI();
-
-	  }	  
-	
-	]) 
+	])
 })(); 
---
+++ employee/internal_consumition/detailconsumition.js
@@ -0,0 +1,15 @@
+(function () {
+	var controllerId = 'app.views.employee.detailconsumition';
+	angular.module('app').controller(controllerId, [
+		'$scope',
+		'gtObject',
+		'gtUI',
+		'gtPlataform',
+		'gtMessage',
+		'gtString',
+		function ($scope, gtObject, gtUI, gtPlataform, gtMessage, gtString) {
+			var vm = this;
+        }
+
+	])
+})(); 
